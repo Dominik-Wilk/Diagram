193f0fa8de3cb02462f591c235a324713acd7e6e
Dominik Wilk <mr.wilkd@gmail.com>
2023-10-17 12:54:52 +0100

    Update createCircles function

--- script.js
+++ script.js
@@ -2,88 +2,205 @@ const container = document.querySelector('.container');
 let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 container.appendChild(svg);
 
+// gchild1_2 === child2_1_2
+// to make tree easer to read in "gchild" I cuted first number from his parent
+
+const tree = {
+  name: 'root',
+  Id: '1',
+  message: '',
+  children: [
+    {
+      name: 'child1',
+      Id: '2',
+      message: '',
+      children: [
+        {
+          name: 'child1_1',
+          Id: '3',
+          message: '',
+          children: null,
+        },
+      ],
+    },
+
+    {
+      name: 'child2',
+      Id: '6',
+      message: '',
+      children: [
+        {
+          name: 'child2_1',
+          Id: '13',
+          message: '',
+          children: [
+            {
+              name: 'gchild1_2',
+              Id: '14',
+              message: '',
+              children: [
+                {
+                  name: 'gchild1_3',
+                  Id: '15',
+                  message: '',
+                  children: [
+                    {
+                      name: 'gchild1_4',
+                      Id: '16',
+                      message: '',
+                      children: null,
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: 'child2_2',
+          Id: '7',
+          message: '',
+          children: [
+            {
+              name: 'gchild2_1',
+              Id: '11',
+              message: '',
+              children: null,
+            },
+            {
+              name: 'gchild2_2',
+              Id: '12',
+              message: '',
+              children: null,
+            },
+          ],
+        },
+        {
+          name: 'child2_2_1',
+          Id: '8',
+          message: '',
+          children: null,
+        },
+        {
+          name: 'child2_2_2',
+          Id: '9',
+          message: '',
+          children: [
+            {
+              name: 'gchild2_2_1',
+              Id: '5',
+              message: '',
+              children: null,
+            },
+            {
+              name: 'gchild2_2_2',
+              Id: '4',
+              message: '',
+              children: null,
+            },
+          ],
+        },
+        {
+          name: 'child2_2_3',
+          Id: '10',
+          message: '',
+          children: null,
+        },
+      ],
+    },
+  ],
+};
+
 const createPaths = () => {
-  for (let i = 0; i <= 17; i++) {
-    const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    path.classList.add(`path${i}`);
-    let dValue;
-    switch (i) {
-      case 0:
-        dValue = 'M 46 50 L 390 50';
-        break;
-      case 1:
-        dValue = 'M 390 50 L 850 50';
-        break;
-      case 2:
-        dValue = 'M 50 50 C 180 40 60 172 150 170';
-        break;
-      case 3:
-        dValue = 'M 150 170 L 220 170';
-        break;
-      case 4:
-        dValue = 'M 220 170 L 400 170';
-        break;
-      case 5:
-        dValue = 'M 400 170 L 610 170';
-        break;
-      case 6:
-        dValue = 'M 610 170 L 950 170';
-        break;
-      case 7:
-        dValue = 'M 230 170 C 388 163 217 274 400 270';
-        break;
-      case 8:
-        dValue = 'M 400 270 L 460 270';
-        break;
-      case 9:
-        dValue = 'M 460 270 C 584 276 462 159 610 170';
-        break;
-      case 10:
-        dValue = 'M 150 170 L 150 370';
-        break;
-      case 11:
-        dValue = 'M 150 370 L 300 370';
-        break;
-      case 12:
-        dValue = 'M 300 370 L 400 370';
-        break;
-      case 13:
-        dValue = 'M 400 370 L 440 370';
-        break;
-      case 14:
-        dValue = 'M 750 113 C 636 114 721 179 610 170';
-        break;
-      case 15:
-        dValue = 'M 850 50 L 850 113';
-        break;
-      case 16:
-        dValue = 'M 850 113 L 750 113';
-        break;
-      case 17:
-        dValue = 'M 850 113 C 926 115 872 167 953 170';
-        break;
-    }
+  // for (let i = 0; i <= 17; i++) {
+  //   const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  //   path.classList.add(`path${i}`);
+  //   let dValue;
+  // switch (i) {
+  //   case 0:
+  //     dValue = 'M 46 50 L 390 50';
+  //     break;
+  //   case 1:
+  //     dValue = 'M 390 50 L 850 50';
+  //     break;
+  //   case 2:
+  //     dValue = 'M 50 50 C 180 40 60 172 150 170';
+  //     break;
+  //   case 3:
+  //     dValue = 'M 150 170 L 220 170';
+  //     break;
+  //   case 4:
+  //     dValue = 'M 220 170 L 400 170';
+  //     break;
+  //   case 5:
+  //     dValue = 'M 400 170 L 610 170';
+  //     break;
+  //   case 6:
+  //     dValue = 'M 610 170 L 950 170';
+  //     break;
+  //   case 7:
+  //     dValue = 'M 230 170 C 388 163 217 274 400 270';
+  //     break;
+  //   case 8:
+  //     dValue = 'M 400 270 L 460 270';
+  //     break;
+  //   case 9:
+  //     dValue = 'M 460 270 C 584 276 462 159 610 170';
+  //     break;
+  //   case 10:
+  //     dValue = 'M 150 170 L 150 370';
+  //     break;
+  //   case 11:
+  //     dValue = 'M 150 370 L 300 370';
+  //     break;
+  //   case 12:
+  //     dValue = 'M 300 370 L 400 370';
+  //     break;
+  //   case 13:
+  //     dValue = 'M 400 370 L 440 370';
+  //     break;
+  //   case 14:
+  //     dValue = 'M 750 113 C 636 114 721 179 610 170';
+  //     break;
+  //   case 15:
+  //     dValue = 'M 850 50 L 850 113';
+  //     break;
+  //   case 16:
+  //     dValue = 'M 850 113 L 750 113';
+  //     break;
+  //   case 17:
+  //     dValue = 'M 850 113 C 926 115 872 167 953 170';
+  //     break;
+  // }
 
-    path.setAttribute('d', dValue);
-    path.setAttribute('stroke', 'white');
-    path.setAttribute('fill', 'transparent');
+  // path.setAttribute('d', dValue);
+  //   path.setAttribute('stroke', 'white');
+  //   path.setAttribute('fill', 'transparent');
 
-    svg.appendChild(path);
-  }
-  createCircles();
+  //   svg.appendChild(path);
+  // }
+  createCircles(tree);
 };
 
-const createCircles = () => {
-  for (let i = 0; i <= 15; i++) {
-    const circle = document.createElement('div');
-    circle.classList.add(`circle`);
-    circle.classList.add(`circle${i}`);
-    container.appendChild(circle);
+const createCircles = tree => {
+  if (tree === null) {
+    return;
   }
-  animationPaths();
+  console.log(tree);
+  const circle = document.createElement('div');
+  circle.classList.add('circle');
+  circle.classList.add(`circle${tree.Id}`);
+  circle.classList.add(`circle${tree.name}`);
+  container.appendChild(circle);
+
+  if (tree.children !== null) {
+    tree.children.forEach(child => {
+      createCircles(child);
+    });
+  }
 };
 
-function animationPaths() {
+const animationPaths = () => {
   let line = anime({
     targets: 'path',
     strokeDashoffset: [anime.setDashoffset, 0],
@@ -100,83 +217,6 @@ function animationPaths() {
       });
     },
   });
-}
+};
 
 createPaths();
-
-// gchild1_2 === child2_1_2
-// to make tree easer to read in "gchild" I cuted first number from his parent
-
-let tree = {
-  name: 'root',
-  children: [
-    {
-      name: 'child1',
-      children: [
-        {
-          name: 'child1_1',
-          children: [],
-        },
-      ],
-    },
-
-    {
-      name: 'child2',
-      children: [
-        {
-          name: 'child2_1',
-          children: [
-            {
-              name: 'gchild1_2',
-              children: [
-                {
-                  name: 'gchild1_3',
-                  children: [
-                    {
-                      name: 'gchild1_4',
-                      children: [],
-                    },
-                  ],
-                },
-              ],
-            },
-          ],
-        },
-        {
-          name: 'child2_2',
-          children: [
-            {
-              name: 'gchild2_1',
-              children: [],
-            },
-            {
-              name: 'gchild2_2',
-              children: [],
-            },
-          ],
-        },
-        {
-          name: 'child2_2_1',
-          children: [],
-        },
-        {
-          name: 'child2_2_2',
-          children: [
-            {
-              name: 'gchild2_2_1',
-              children: [],
-            },
-            {
-              name: 'gchild2_2_2',
-              children: [],
-            },
-          ],
-        },
-        {
-          name: 'child2_2_3',
-          children: [],
-        },
-      ],
-    },
-  ],
-};
